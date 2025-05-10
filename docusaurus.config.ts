4ee5f1138f6b2d21aa57bf179646e3f0a1e5c3e2
Rohit685 <mittalr685@gmail.com>
2025-05-10 12:26:38 -0500

    fix config maybe

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,16 @@ const config: Config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
+    url: 'https://policing-redefined.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'Policing-Redefined', // Usually your GitHub org/user name.
-    projectName: 'docs', // Usually your repo name.
+    organizationName: 'policing-redefined', // Usually your GitHub org/user name.
+    projectName: 'policing-redefined.github.io', // Usually your repo name.
+    trailingSlash:false,
 
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
