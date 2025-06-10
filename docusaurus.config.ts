076c80f27a4acf9cd623699cca53bc6ef6fade61
Sprayxe <sprayxe.marcel@gmail.com>
2025-06-10 21:47:40 +0200

    fixed embed

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
 
     themeConfig: {
         // Replace with your project's social card
-        image: 'img/docusaurus-social-card.jpg',
+        image: 'img/PRBanner.png',
         navbar: {
             title: 'Policing Redefined',
             logo: {
